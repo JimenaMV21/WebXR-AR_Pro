30541bc9ac0de3831c45c79870285366654af067
JimenaMV21 <jimena.moralesvi@anahuac.mx>
2024-09-22 23:29:55 -0600

    First

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076189.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196833059, 196833060, 196833083, 196833063, 196833064, 196833065, 196833066, 196833067, 196833069, 196833070, 196833071, 196833072, 196833074, 196833075 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196833080/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196833081/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196833079/1/ammo.js', 'preload' : true},
+];
